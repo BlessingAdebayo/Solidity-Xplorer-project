56de67f858294c5a6e7aca8dad4150bd94e7ab35
BlessingAdebayo <adebayoblessing407@gmail.com>
2022-11-16 03:26:46 +0100

    contract update

--- smb-smart-contract-ethereum/SmartInvoice.sol
+++ smb-smart-contract-ethereum/SmartInvoice.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.4.21 < 0.8.17
 contract SimplePaymentChannel {
     address payable public sender;      // The account sending payments.
     address payable public recipient;   // The account receiving the payments.
